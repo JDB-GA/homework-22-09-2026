cecc556424fbc88689a2b7cfb3332da562eeaee1
almutawaj <muntadher.contact@gmail.com>
2026-09-22 15:40:53 +0300

    1. Repo & Model

---
+++ src/main/java/com/ga/todo/model/Category.java
@@ -0,0 +1,32 @@
+package com.ga.todo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
+
+
